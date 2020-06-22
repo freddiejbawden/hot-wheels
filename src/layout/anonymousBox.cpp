c9339c8fa02be5382850095d153c2b45d65d76f2
freddiejbawden <freddiejbawden@gmail.com>
2020-06-22 15:18:37 +0100

    Inline elements expand parent when too large

--- src/layout/anonymousBox.cpp
+++ src/layout/anonymousBox.cpp
@@ -17,18 +17,20 @@ void AnonymousBox::calculatePosition(Dimensions parent) {
 void AnonymousBox::calculateChildren() {
   Dimensions lineBox = dimensions;
   int xBound = lineBox.content.x + lineBox.content.width;
-  bool first = true;
+  bool wrapped = true;
   for (std::vector<LayoutBox*>::iterator it = children.begin(); it != children.end(); ++it) {
     LayoutBox* child = (*it);
     // Maybe handle split in this function? return a split child?
     child->createLayout(lineBox);
     Dimensions childDims = child->dimensions;
     if(lineBox.content.x + childDims.content.width > xBound)  {
-      std::cout << "recalculating\n";
+      std::cout << "recalculating " << childDims.content.height << '\n';
       // handle split and make a new line box
       // TODO: account for padding
       lineBox.content.x = dimensions.content.x;
-      dimensions.content.height += childDims.content.height;
+      lineBox.content.y = dimensions.content.height + dimensions.content.y;
+      wrapped = true;
+      std::cout << dimensions.content.height << " new height \n";
       // doing this twice is ineffcient, we will probably need to 
       // make createLayout(..) an interface 
       child->createLayout(lineBox);
@@ -36,7 +38,10 @@ void AnonymousBox::calculateChildren() {
       // there is space to spare
       lineBox.content.x += childDims.content.width;
     }
-    dimensions.content.height = childDims.content.height;
+    if (wrapped) {
+      dimensions.content.height += childDims.content.height;
+      wrapped = false;
+    }
   }
 };
 void AnonymousBox::calculateHeight() {
